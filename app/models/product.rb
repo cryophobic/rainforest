963e7c772b2aa7cc87141edb59b3b6964a574dcc
kevlarmusik <kevlar.eq@gmail.com>
2013-09-10 19:09:23 -0400

    Writing the review routes, controllers and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,11 @@ class Product < ActiveRecord::Base
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => {:only_integer => true}
+  has_many :reviews
+  has_many :users, :through => :reviews
 
   def formatted_price
-    price_in_dollars = price_in_cents.to_f / 100
-    sprintf("%.2f", price_in_dollars)
+    (self.price_in_cents.to_f / 100).to_s(:currency)
   end
-  
+
 end
